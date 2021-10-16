fb07dd6dff7abcab30aeeac2f7632b75f33068f1
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2021-10-16 19:39:14 +0500

    updated admin.py of books_app

--- books_app/models.py
+++ books_app/models.py
@@ -7,3 +7,6 @@ class Book(models.Model):
     book_name = models.CharField(max_length=30, default="blank")
     book_code = models.CharField(max_length=10, default="default0")
     quantity_available = models.IntegerField()
+
+    def __str__(self):
+        return self.book_code
