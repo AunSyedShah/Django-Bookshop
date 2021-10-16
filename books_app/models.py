275b5f483946f50d78df86a58ace2c0845885952
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2021-10-16 19:27:11 +0500

    updated model for books_apps

--- books_app/models.py
+++ books_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Book(models.Model):
+    book_name = models.CharField(max_length=30, default="blank")
+    book_code = models.CharField(max_length=10, default="default0")
+    quantity_available = models.IntegerField()
