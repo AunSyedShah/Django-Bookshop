ccb1c47aa6e524cdc80db8335f68d2c04bf5660f
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2021-10-17 16:13:02 +0500

    added model form and create operation for books app

--- books/views.py
+++ books/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import BookForm
+from django.http import HttpResponse
+
 
 # Create your views here.
+def create_book(request):
+    form = BookForm()
+    context = {
+        'form': form
+    }
+    if request.method == 'POST':
+        form_data = BookForm(request.POST)
+        if form_data.is_valid():
+            form_data.save()
+            return HttpResponse("<p>Form Submitted Successfully</p>")
+    else:
+        return render(request, "book.html", context)
