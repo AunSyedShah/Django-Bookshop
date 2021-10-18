40752015e6444dd60b77530d38083236b7d58593
Umair <umairapi1995@gmail.com>
2021-10-18 16:40:09 +0500

    remove extra test user model

--- account/models.py
+++ account/models.py
@@ -28,27 +28,3 @@ class User(AbstractBaseUser, PermissionsMixin):
         full_name = self.first_name + " " + self.last_name
         return full_name.strip()
 
-
-
-
-
-
-
-# class User(AbstractBaseUser, PermissionsMixin):
-#     email = models.EmailField(max_length=254, unique=True)
-#     name = models.CharField(max_length=254, null=True, blank=True)
-#     is_staff = models.BooleanField(default=False)
-#     is_superuser = models.BooleanField(default=False)
-#     is_active = models.BooleanField(default=True)
-#     last_login = models.DateTimeField(null=True, blank=True)
-#     date_joined = models.DateTimeField(auto_now_add=True)
-    
-
-#     USERNAME_FIELD = 'email'
-#     EMAIL_FIELD = 'email'
-#     REQUIRED_FIELDS = []
-
-#     objects = UserManager()
-
-#     def get_absolute_url(self):
-#         return "/users/%i/" % (self.pk)
