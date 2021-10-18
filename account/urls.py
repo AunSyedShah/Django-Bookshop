305e5b90268e9b492644dff16ef1b90ef3aa6e2d
Umair <umairapi1995@gmail.com>
2021-10-18 22:44:48 +0500

    url for login using LoginAuth view

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from account.views import register
-
+from django.contrib.auth import views as auth_views
+from account.forms import CustomAuthForm
 urlpatterns = [
-    path("account/register/", register, name="register")
+    path("account/register/", register, name="register"),
+    path('account/login/',
+        auth_views.LoginView.as_view(template_name="account/login.html", authentication_form = CustomAuthForm),
+        name="login",
+        )
 ]
