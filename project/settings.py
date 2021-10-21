7451f8c67ef1880c95250a899fc0c3674b818737
umair313 <umairapi1995@gmail.com>
2021-10-21 14:35:18 +0500

    get EMAIL_HOST_PASSWORD from .env

--- project/settings.py
+++ project/settings.py
@@ -137,6 +137,6 @@ LOGOUT_REDIRECT_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = 'wttsuuejmcabaduu'
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
