ae2f326a5682a8466364dfbba284d62aaca03d8c
umair313 <umairapi1995@gmail.com>
2021-10-21 11:10:44 +0500

    path for password reset view

--- account/urls.py
+++ account/urls.py
@@ -2,13 +2,18 @@ from django.urls import path
 from account.views import register
 from django.contrib.auth import views as auth_views
 
-from account.forms import CustomAuthForm
+from account.forms import CustomAuthForm, CustomPasswordResetForm
 urlpatterns = [
     path("account/register/", register, name="register"),
     path('account/login/',
         auth_views.LoginView.as_view(template_name="account/login.html", authentication_form = CustomAuthForm),
         name="login",
         ),
-    path("account/logout/", auth_views.LogoutView.as_view(),name="logout")
+    path('password-reset', 
+        auth_views.PasswordResetView.as_view(template_name="account/password-reset.html", form_class = CustomPasswordResetForm),
+        name="password-reset-request"),
+
+    path("account/logout/", auth_views.LogoutView.as_view(),name="logout"),
+    
     
 ]
