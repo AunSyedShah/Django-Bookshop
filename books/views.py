d933557ccdf4045d2ec1b32ce19494c25ee7eb34
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2021-10-25 00:23:52 +0500

    enhance addBook.html

--- books/views.py
+++ books/views.py
@@ -4,13 +4,13 @@ from .forms import AddBookForm
 
 # Create your views here.
 def add_book(request):
+    context = {}
     if request.method == "POST":
         form = AddBookForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request, "books/addBook.html")
+            context['request'] = request
+            return render(request, "books/addBook.html", context)
     add_book_form = AddBookForm()
-    context = {
-        'form': add_book_form
-    }
+    context["form"] = add_book_form
     return render(request, "books/addBook.html", context)
