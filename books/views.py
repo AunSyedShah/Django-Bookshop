4204bb1ce2db0df48bf0c5d2e2318f38cc391500
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2021-10-25 00:18:50 +0500

    update view.py for post request

--- books/views.py
+++ books/views.py
@@ -4,6 +4,11 @@ from .forms import AddBookForm
 
 # Create your views here.
 def add_book(request):
+    if request.method == "POST":
+        form = AddBookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "books/addBook.html")
     add_book_form = AddBookForm()
     context = {
         'form': add_book_form
